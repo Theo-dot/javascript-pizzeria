749e22ebbdc8e8907b36267086e4e3e9a5d4ae1d
Łukasz Spalczyński <spali89@gmail.com>
2019-08-08 18:32:45 +0200

    change package.json file

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,13 +1,15 @@
-import {select, templates} from '../settings.js';
+import {select, templates, settings} from '../settings.js';
 import {AmountWidget} from './AmountWidget.js';
 import {DatePicker} from './DatePicker.js';
 import {HourPicker} from './HourPicker.js';
+import {utils} from '../utils.js';
 
 export class Booking {
   constructor() {
     const thisBooking = this;
     thisBooking.render();
     thisBooking.initWidgets();
+    thisBooking.getData();
   }
 
   render() {
@@ -24,8 +26,12 @@ export class Booking {
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(
       select.booking.hoursAmount
     );
-    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
-    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(
+      select.widgets.datePicker.wrapper
+    );
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(
+      select.widgets.hourPicker.wrapper
+    );
   }
 
   initWidgets() {
@@ -34,5 +40,84 @@ export class Booking {
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
     thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+  }
+
+  getData() {
+    const thisBooking = this;
+
+    const startEndDates = {};
+    startEndDates[settings.db.dateStartParamKey] = utils.dateToStr(
+      thisBooking.datePicker.minDate
+    );
+    startEndDates[settings.db.dateEndParamKey] = utils.dateToStr(
+      thisBooking.datePicker.maxDate
+    );
+
+    const endDate = {};
+    endDate[settings.db.dateEndParamKey] =
+      startEndDates[settings.db.dateEndParamKey];
+
+    const params = {
+      booking: utils.queryParams(startEndDates),
+      eventsCurrent:
+        settings.db.notRepeatParam + '&' + utils.queryParams(startEndDates),
+      eventsRepeat: settings.db.repeatParam + '&' + utils.queryParams(endDate),
+    };
+
+    console.log('getData params', params);
+
+    const urls = {
+      booking:
+        settings.db.url + '/' + settings.db.booking + '?' + params.booking,
+      eventsCurrent:
+        settings.db.url + '/' + settings.db.event + '?' + params.eventsCurrent,
+      eventsRepeat:
+        settings.db.url + '/' + settings.db.event + '?' + params.eventsRepeat,
+    };
+
+    console.log('getData urls', urls);
+
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(
+        [bookingsResponse, eventsCurrentResponse, eventsRepeatResponse]
+      ) {
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]) {
+        thisBooking.parseData(bookings, eventsCurrent, eventsRepeat);
+      });
+  }
+
+  parseData(bookings, eventsCurrent, eventsRepeat) {
+    const thisBooking = this;
+    thisBooking.booked = {};
+
+    for (let i = 0; i < eventsCurrent.length; i++) {
+      const {date, hour, duration, table} = eventsCurrent[i];
+      console.log('CURRENT', eventsCurrent[i]);
+      thisBooking.makeBooked(date, hour, duration, table);
+    }
+  }
+
+  makeBooked(date, hour, duration, table) {
+    const thisBooking = this;
+    const newHour = utils.hourToNumber(hour);
+    console.log('new Hour', newHour);
+    console.log('duration', duration);
+
+    //for (let i = 0; i < duration; i++) {
+      thisBooking.booked[date] = {};
+      thisBooking.booked[date][newHour] = [table];
+
+      console.log('thisBOOKING.BOOKED', thisBooking.booked);
+    //}
   }
 }
